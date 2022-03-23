e627f5e66a3347f8e4c8ee5e503c28811c3b6473
Tom Gamon <tom@tomgamon.com>
2022-03-23 16:40:13 +1100

    Deploy on fly

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 
   srv := &http.Server{
     Handler: handlers.CombinedLoggingHandler(os.Stdout, r),
-    Addr:    "127.0.0.1:8080",
+    Addr:    "0.0.0.0:8080",
     WriteTimeout: 15 * time.Second,
     ReadTimeout:  15 * time.Second,
   }
