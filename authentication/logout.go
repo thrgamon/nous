a95873cb248b185be3431273c2aff8ad3772bcba
Tom Gamon <tom@tomgamon.com>
2022-04-01 15:13:14 +1100

    Add a comment

--- authentication/logout.go
+++ authentication/logout.go
@@ -30,6 +30,7 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 	parameters.Add("client_id", os.Getenv("AUTH0_CLIENT_ID"))
 	logoutUrl.RawQuery = parameters.Encode()
 
+  // Immediately expire the cookie
   sessionState, _ := Store.Get(r, "auth")
   sessionState.Options.MaxAge = -1
   sessionState.Save(r, w)
