6d4869a557267f72994db07d75014c76f3eba6b6
Tom Gamon <tom@tomgamon.com>
2022-04-08 15:37:28 +1000

    Add materialized view for search

---
+++ db/migrations/000006_create_search_view.down.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER refresh_resource_search on posts;
+DROP TRIGGER refresh_resource_search on votes;
+DROP TRIGGER refresh_resource_search on tags;
+DROP FUNCTION refresh_resource_search();
+DROP MATERIALIZED VIEW resource_search;
