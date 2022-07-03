aa525d355c6f92f147f322a79564b15c03967024
Tom Gamon <tom@tomgamon.com>
2022-07-04 09:39:11 +1000

    Add iframes for links

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var checkboxes = document.querySelectorAll("input[name=done]");
+const checkboxes = document.querySelectorAll("input[name=done]");
 
 checkboxes.forEach(checkbox => {
   checkbox.addEventListener('change', (event) => {
@@ -15,6 +15,25 @@ checkboxes.forEach(checkbox => {
     event.preventDefault()
   });
 });
+
+const noteContent = document.querySelectorAll("[data-note-content]")
+
+for (i = 0; i < noteContent.length; i++) {
+  const note = noteContent[i]
+  const links = note.querySelectorAll('a')
+
+  if (links.length == 0) {
+    continue
+  } else {
+    links.forEach(link => {
+      const details = document.createElement("details")
+      const iframe = document.createElement("iframe")
+      iframe.src = link.href
+      details.append(iframe)
+      note.append(details)
+    })
+  }
+}
 
 // Example POST method implementation:
 async function postData(url = '', data = {}, errorCallback) {
