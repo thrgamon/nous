1cda01aee834f838ec091f6d80f4b030f87b1860
Tom Gamon <tom@tomgamon.com>
2022-10-05 08:56:44 +1100

    Baby steps with sveltekit

---
+++ nous/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  fetch('http://localhost:8080/api/notes')
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
